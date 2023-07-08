f3e803e09868c8bae306a3c709eb3b6074d5a976
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-07-08 16:54:51 +0900

    feat: 주문 취소 또는 배송지 변경 API 추가

--- src/router/orderRouter.js
+++ src/router/orderRouter.js
@@ -1,41 +1,38 @@
 const { Router } = require("express");
 const orderService = require("../services/orderService");
+const { isAuthenticated, asyncHandler } = require("../middlewares");
 const utils = require("../misc/utils");
 const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
     const id = req.body.userId;
-    const id2 = req.currentUserId;
-    console.log('도니?');
-    console.log(id);
-    console.log(id2);
+    // const id2 = req.currentUserId;
     const orderInfo = await orderService.getOrderInfo(id);
-    // res.status(201).json(orderInfo);
     res.json(utils.buildResponse(orderInfo));
 
-    // const myOrders = await UserService.getMyInfo(id).orderList;
-    // console.log(myOrders);
-    // const products = [];
-    // myOrders.forEach(async el => {
-    //     // 배열에 있는 주문id를 하나씩 foreach 돌려서 배열에 담는다.
-    //     let data = await orderService.getOrderInfo(el);
-    //     products.push(data);
-    // })
-    // // 담은 배열은 주문id로 이루어진 배열이다.
   } catch (err) {
     next(err);
   }
 });
 
 // 주문 취소
-router.put("/:orderId", async (req, res, next) => {
+router.put("/:orderId", isAuthenticated, async (req, res, next) => {
   try {
-    const id = req.currentUserId;
+    // id 이중검증 필요..?
     const { orderId } = req.params;
-    // 주소데이터가 안들어오면 배송상태 수정. pending 추가?
-    const param = "pending"; // address ?? "pending";
-    const data = await orderService.putOrderCancel(orderId, { param });
+    const { address, flag } = req.body;
+    let param;
+    if(flag) {
+      // 주문 취소
+      param = {status: "pending"};
+    }
+    else {
+      // 배송지 변경
+      param = {address};
+    }
+
+    const data = await orderService.putOrder(orderId, param);
     res.json(utils.buildResponse(data));
   } catch (err) {
     next(err);

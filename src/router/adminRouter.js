20a09ddf3823e348aca582e5b28f464122c63e6e
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-07-09 16:57:29 +0900

    fix: layer에 맞는 명칭으로 수정 및 메서드 병합 또는 분리

--- src/router/adminRouter.js
+++ src/router/adminRouter.js
@@ -1,105 +1,147 @@
-const { Router } = require("express");
-const adminService = require("../services/adminService");
-const utils = require("../misc/utils");
-const { asyncHandler } = require("../middlewares");
+const { Router } = require('express');
+const adminService = require('../services/adminService');
+const utils = require('../misc/utils');
+const { asyncHandler } = require('../middlewares');
 const router = Router();
 
 // 상품 등록
-router.post("/products", asyncHandler(async (req, res, next) => {
-  const { id, name, categoryId, price, subImage, keyWord, description, mainImage } = req.body;
-  const product = await adminService.addProduct({
-    id, name, categoryId, price, subImage, keyWord, description, mainImage
-  });
-  res.json(utils.buildResponse(product));
-})
+router.post(
+  '/products',
+  asyncHandler(async (req, res, next) => {
+    const { id, name, categoryId, price, subImage, keyWord, description, mainImage } = req.body;
+    const product = await adminService.addProduct({
+      id,
+      name,
+      categoryId,
+      price,
+      subImage,
+      keyWord,
+      description,
+      mainImage,
+    });
+    res.json(utils.buildResponse(product));
+  })
 );
 
 // 특정 상품 정보 가져오기 (수정페이지 접근시)
-router.get("/products/:id", asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const product = await adminService.getProduct(id);
-  console.log(product);
-  res.json(utils.buildResponse(product));
-})
+router.get(
+  '/products/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await adminService.getProduct(id);
+    console.log(product);
+    res.json(utils.buildResponse(product));
+  })
 );
 
 // 상품 정보 변경하기 (수정)
-router.put("/products/:id", asyncHandler(async (req, res, next) => {
-  const { id, name, categoryId, price, subImage, keyWord, description, mainImage } = req.body;
-  const product = await adminService.putProduct({
-    id, name, categoryId, price, subImage, keyWord, description, mainImage
-  });
-  res.json(utils.buildResponse(product));
-})
+router.put(
+  '/products/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id, name, categoryId, price, subImage, keyWord, description, mainImage } = req.body;
+    const product = await adminService.putProduct({
+      id,
+      name,
+      categoryId,
+      price,
+      subImage,
+      keyWord,
+      description,
+      mainImage,
+    });
+    res.json(utils.buildResponse(product));
+  })
 );
 
 // 상품 단건 삭제하기
-router.delete("/products/:id", asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const product = await adminService.deleteProduct(id);
-  res.json(utils.buildResponse(product));
-})
+router.delete(
+  '/products/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await adminService.deleteProduct(id);
+    res.json(utils.buildResponse(product));
+  })
 );
 
 // 주문 내역 확인하기 (사용자 전체)
-router.get("/orders", asyncHandler(async (req, res, next) => {
-  const orders = await adminService.getOrders();
-  res.json(utils.buildResponse(orders));
-})
+router.get(
+  '/orders',
+  asyncHandler(async (req, res, next) => {
+    const orders = await adminService.getOrders();
+    res.json(utils.buildResponse(orders));
+  })
 );
 
 // 주문 내역 확인하기 (특정 사용자)
-router.get("/orders/:id", asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const order = await adminService.getOrder(id);
-  res.json(utils.buildResponse(order));
-})
+router.get(
+  '/orders/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const order = await adminService.getOrder(id);
+    res.json(utils.buildResponse(order));
+  })
 );
 
 // 특정 주문 내역 삭제하기 (orderId)
-router.delete("/orders/:id", asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const order = await adminService.deleteOrder(id);
-  res.json(utils.buildResponse(order));
-})
+router.delete(
+  '/orders/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const order = await adminService.deleteOrder(id);
+    res.json(utils.buildResponse(order));
+  })
 );
 
 // 카테고리 추가
-router.post('/categories', asyncHandler(async (req, res, next) => {
-  const { id, name } = req.body;
-  const newCategory = await adminService.addCategory({ id, name });
-  res.json(utils.buildResponse(newCategory));
-}));
+router.post(
+  '/categories',
+  asyncHandler(async (req, res, next) => {
+    const { id, name } = req.body;
+    const newCategory = await adminService.addCategory({ id, name });
+    res.json(utils.buildResponse(newCategory));
+  })
+);
 
 //카테고리 조회
-router.get("/categories/:id", asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const category = await adminService.getCategory(id);
-  res.json(utils.buildResponse(category))
-}));
+router.get(
+  '/categories/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const category = await adminService.getCategory(id);
+    res.json(utils.buildResponse(category));
+  })
+);
 
 //카테고리 수정
-router.put("/categories/:id", asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  const category = await adminService.putCategory({ id, name });
-  res.json(utils.buildResponse(category));
-}))
+router.put(
+  '/categories/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const category = await adminService.putCategory({ id, name });
+    res.json(utils.buildResponse(category));
+  })
+);
 
 //카테고리 삭제
-router.delete('/categories/:id', asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const category = await adminService.deleteCategory(id);
-  res.json(utils.buildResponse(category))
-}))
+router.delete(
+  '/categories/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const category = await adminService.deleteCategory(id);
+    res.json(utils.buildResponse(category));
+  })
+);
 
 // 주문상태 수정
-router.put("/orders/:id", asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const { status } = req.body;
-  const order = await adminService.putStatus(id, status);
-  res.json(utils.buildResponse(order));
-})
+router.put(
+  '/orders/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const { status } = req.body;
+    const order = await adminService.updateStatus(id, status);
+    res.json(utils.buildResponse(order));
+  })
 );
 
 module.exports = router;
--- src/router/orderRouter.js
+++ src/router/orderRouter.js
@@ -1,61 +1,74 @@
-const { Router } = require("express");
-const orderService = require("../services/orderService");
-const { isAuthenticated, asyncHandler } = require("../middlewares");
-const utils = require("../misc/utils");
+const { Router } = require('express');
+const jwt = require('jsonwebtoken');
+const orderService = require('../services/orderService');
+const { isAuthenticated, asyncHandler } = require('../middlewares');
+const utils = require('../misc/utils');
 const router = Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const id = req.body.userId;
-    // const id2 = req.currentUserId;
-    const orderInfo = await orderService.getOrderInfo(id);
-    res.json(utils.buildResponse(orderInfo));
-
-  } catch (err) {
-    next(err);
-  }
+// 사용자의 주문 전체 정보 (목록)
+router.get('/', isAuthenticated, async (req, res, next) => {
+  const userId = req.userId;
+  console.log(userId);
+  const orders = await orderService.getOrders(userId);
+  res.json(utils.buildResponse(orders));
 });
+
+// 사용자의 특정 주문 정보 (상세)
+router.get(
+  '/:id',
+  isAuthenticated,
+  asyncHandler(async (req, res, next) => {
+    const userId = req.userId;
+    const { id } = req.params;
+    const order = await orderService.getOrder(userId, id);
+    res.json(utils.buildResponse(order));
+  })
+);
 
 // 주문 취소
-router.put("/:orderId", isAuthenticated, async (req, res, next) => {
-  try {
-    // id 이중검증 필요..?
-    const { orderId } = req.params;
-    const { address, flag } = req.body;
-    let param;
-    if(flag) {
-      // 주문 취소
-      param = {status: "pending"};
+router.put(
+  '/:orderId',
+  isAuthenticated,
+  asyncHandler(async (req, res, next) => {
+    try {
+      // id 이중검증 필요..?
+      const { orderId } = req.params;
+      const { address, isOrderCancel } = req.body;
+      let data;
+      if (isOrderCancel) {
+        // 주문 취소
+        data = await orderService.putOrder(orderId, { status: 'pending' });
+      } else {
+        // 배송지 변경
+        data = await orderService.putOrder(orderId, { address, receiver });
+      }
+      res.json(utils.buildResponse(data));
+    } catch (err) {
+      next(err);
     }
-    else {
-      // 배송지 변경
-      param = {address};
+  })
+);
+
+// 주문하기
+router.post(
+  '/',
+  isAuthenticated,
+  asyncHandler(async (req, res, next) => {
+    try {
+      const { id, items, itemTotal, userId, address, receiver, status } = req.body;
+
+      if (!id || !items || !itemTotal || !userId || !address || !receiver || !status) {
+        throw new Error('필수 정보를 모두 입력해주세요.');
+      }
+      const data = await orderService.postOrder({ id, items, itemTotal, userId, address, receiver, status });
+
+      res.json(utils.buildResponse(data));
+
+      return data;
+    } catch (err) {
+      next(err);
     }
-
-    const data = await orderService.putOrder(orderId, param);
-    res.json(utils.buildResponse(data));
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post("/", async (req, res, next) => {
-  try {
-    const { id, items, itemTotal, userId, address, receiver, status } = req.body;
- 
-    if (!id || !items || !itemTotal || !userId || !address || !receiver || !status) {
-      throw new Error("필수 정보를 모두 입력해주세요.");
-    }
-    
-    const data = await orderService.postOrder({ id, items, itemTotal, userId, address, receiver, status });
-
-    res.json(utils.buildResponse(data));
-
-    return data;
-  } catch (err) {
-    next(err);
-  }
-});
-
+  })
+);
 
 module.exports = router;

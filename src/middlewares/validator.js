c8e552267422a5e28a5ca76f5f2b7614fee07690
[SW5] 김성진 <kimsungjin927@gmail.com>
2023-07-09 16:24:42 +0900

    feat: 유효성 검증 미들웨어 수정

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,8 +1,8 @@
 const { validationResult, body } = require('express-validator');
 
-const idCheck = body("id", "Id: 최소 8자, 최대 12자, 영소문자 및 숫자만 포함되어야 합니다.").notEmpty().matches(/^[a-z0-9]{8,12}$/);
+const idCheck = body("id").notEmpty().matches(/^[a-z0-9]{8,12}$/).withMessage("Id: 최소 8자, 최대 12자, 영소문자 및 숫자만 포함되어야 합니다");
 
-const passwordCheck = body("pw", "Pw: 최소 8자, 하나 이상의 대소문자 및 하나의 숫자, 하나의 특수문자로 구성되어야 합니다.").notEmpty().matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/);
+const passwordCheck = body("pw").notEmpty().matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/).withMessage("Pw: 최소 8자, 하나 이상의 대소문자 및 하나의 숫자, 하나의 특수문자로 구성되어야 합니다");
 
 const loginCheck = [
     idCheck,
@@ -31,9 +31,10 @@ const meCheck = [
 ];
 
 const validatorError = (req, res, next) => {
-    const Err = validationResult(req);
-    if (!Err.isEmpty()) {
-        throw new Error(Err.errors[0].msg);
+    const errors = validationResult(req).errors;
+    if (Object.keys(errors).length !== 0) {
+        let messages = errors.map(e => e.msg);
+        throw new Error(messages)
     }
     next();
 }

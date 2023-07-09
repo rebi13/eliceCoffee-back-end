5601bfa5fdbb330418cf80d9fac84d1866b22102
[SW5] 김성진 <kimsungjin927@gmail.com>
2023-07-09 11:55:35 +0900

    Refactor: hash, randompw util 폴더로 옮김

--- src/middlewares/hashPassword.js
+++
@@ -1,3 +0,0 @@
-const crypto = require("crypto");
-
-module.exports = (pw) => crypto.createHash("sha256").update(pw).digest("hex");
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,13 +1,9 @@
 const userMiddleware = require("./userMiddleware");
-const hashPassword = require("./hashPassword");
 const asyncHandler = require("./asyncHandler");
-const randomPassword = require("./randomPassword");
 const isAuthenticated = require("./isAuthenticated");
 
 module.exports = {
   userMiddleware,
-  hashPassword,
   asyncHandler,
-  randomPassword,
   isAuthenticated,
 };

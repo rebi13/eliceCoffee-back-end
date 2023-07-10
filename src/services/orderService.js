88ec45025d5be2066ddbc9abd0670759d403899e
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-07-10 15:18:11 +0900

    fix: 계층별 용어 사용 및 수정

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -4,9 +4,7 @@ class OrderService {
   constructor(orderModel) {
     this.orderModel = orderModel;
   }
-  // async getOrderInfo(userId) {
-  //   return await this.orderModel.findByUserId(userId);
-  // }
+
   // 주문 내역 전체 조회 사용자
   async getOrders(userId) {
     const orders = await this.orderModel.findOrders(userId);
@@ -19,21 +17,17 @@ class OrderService {
     return order;
   }
 
+  // 주문 내역 변경 사용자
   async putOrder(orderId, { address, receiver }) {
     return await this.orderModel.updateOrder(orderId, { address, receiver });
   }
+
   // 주문 내역 주문상태 변경 사용자
   async putStatus(id, status) {
     const result = await this.orderModel.updateStatus(id, status);
     return result;
   }
 
-  // async putOrderCancel(orderId) {
-  //   return await this.orderModel.cancelOrder(orderId);
-  // }
-  // async putOrderAddress(orderId) {
-  //   return await this.orderModel.updateAddress(orderId);
-  // }
   async postOrder(orderInfo) {
     return await this.orderModel.createOrder(orderInfo);
   }

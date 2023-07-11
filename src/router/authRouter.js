9dc6621d6ca416f47c7bc939d88e2f96bcc7c423
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-07-11 12:17:20 +0900

    fix: 로그인 성공 시 token 값 보내줌

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -21,7 +21,8 @@ router.post(
   asyncHandler(async (req, res, next) => {
     const { id, pw } = req.body;
     const userToken = await userService.getUserToken({ id, pw });
-    res.cookie('loginToken', userToken).json(buildResponse({ isLogin: true }));
+    // res.cookie('loginToken', userToken).json(buildResponse({ isLogin: true }));
+    res.json(buildResponse({ msg: '로그인 성공', loginToken: userToken }));
   })
 );
 

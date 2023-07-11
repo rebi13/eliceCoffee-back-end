fb099fe508d77b05ad0b07e117da07ef289c85a3
[SW5] 김성진 <kimsungjin927@gmail.com>
2023-07-11 17:34:47 +0900

    feat: 관리자 여부 확인하는 미들웨어 추가

--- src/middlewares/isAuthenticated.js
+++ src/middlewares/isAuthenticated.js
@@ -9,7 +9,6 @@ module.exports = (req, res, next) => {
     }
     jwt.verify(Token, process.env.JWT_SECRET_KEY, (err, decoded) => {
       req.userId = decoded.id;
-      req.role = decoded.role;
     });
     next();
   } catch (error) {

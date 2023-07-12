cfa63f9fdc59dd4559e4c6a114ec3f65e39ab742
[SW5] 김성진 <kimsungjin927@gmail.com>
2023-07-12 06:11:45 +0000

    hotfix: 철자 바꾸기

--- src/db/models/ProductModel.js
+++ src/db/models/ProductModel.js
@@ -29,7 +29,7 @@ class ProductModel {
     return (await Product.find({ categoryId }).sort({ "createdAt": -1 })).slice(0, 6);
   }
   async findSixCoffee() {
-    return (await Product.find({ categoryId: { $ne: 'Supplies' } }).sort({ "createdAt": -1 })).slice(0, 6);
+    return (await Product.find({ categoryId: { $ne: 'supplies' } }).sort({ "createdAt": -1 })).slice(0, 6);
   }
 }
 
